78f2f0b415175c9ed6d084abdc71ae66127a7bd7
Chad <chadracelis@yahoo.com>
2021-03-13 11:40:56 -0500

    Didsessions - Rm useless files and general cleanup

--- src/app/didsessions/components/components.module.ts
+++ src/app/didsessions/components/components.module.ts
@@ -3,19 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { CreatePasswordComponent } from './createpassword/createpassword.component';
-import { ImportDIDSourceComponent } from './importdidsource/importdidsource.component';
 import { MnemonicPassCheckComponent } from './mnemonicpasscheck/mnemonicpasscheck.component';
-import { EmptyImportedDocumentComponent } from './emptyimporteddocument/emptyimporteddocument.component';
-import { DIDButtonComponent } from './did-button/did-button.component';
 
 @NgModule({
   declarations: [
-    CreatePasswordComponent,
-    ImportDIDSourceComponent,
     MnemonicPassCheckComponent,
-    EmptyImportedDocumentComponent,
-    DIDButtonComponent
   ],
   imports: [
     CommonModule,
@@ -24,21 +16,12 @@ import { DIDButtonComponent } from './did-button/did-button.component';
     TranslateModule
   ],
   exports: [
-    CreatePasswordComponent,
-    ImportDIDSourceComponent,
     MnemonicPassCheckComponent,
-    EmptyImportedDocumentComponent,
-    DIDButtonComponent
   ],
   providers: [
-    //AdvancedPopupController
   ],
   entryComponents: [
-    CreatePasswordComponent,
-    ImportDIDSourceComponent,
     MnemonicPassCheckComponent,
-    EmptyImportedDocumentComponent,
-    DIDButtonComponent
   ],
 })
 export class ComponentsModule { }
