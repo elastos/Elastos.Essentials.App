b5b00e0a8b91aea778dc735c2f5487d6eb4a85e6
Chad <chadracelis@yahoo.com>
2021-04-13 11:44:58 -0400

    #7bwz73 Didsessions - Update onboard

--- src/assets/translations/didsessions/en.ts
+++ src/assets/translations/didsessions/en.ts
@@ -32,6 +32,7 @@ export const en = {
   'import-again-yes': 'Yes',
 
   // Create screen
+  'elastos-essentials': 'Elastos Essentials',
   'welcome-message1': 'Welcome to the modern internet, where you take control of your own digital identity in a secured sand-box environment.',
   'welcome-message2': 'This will give you the ability to verify your identity and its credentials seamlessly across all applications without losing your privacy and data.',
   'welcome-message3': "Let's start by creating your first decentralized identity or import one that you already own.",
