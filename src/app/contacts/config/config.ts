0d773e79d2345737bc409f77fdb7935d3fcc02c2
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2021-07-23 22:30:29 +0800

    Added more default contacts (tuum tech, donnie, michael S)

--- src/app/contacts/config/config.ts
+++ src/app/contacts/config/config.ts
@@ -1,5 +1,8 @@
 export const defaultContacts = [
-  'did:elastos:iXyYFboFAd2d9VmfqSvppqg1XQxBtX9ea2', // Trinity Tech
   'did:elastos:iabbGwqUN18F6YxkndmZCiHpRPFsQF1imT', // Chen Rong
-  'did:elastos:insTmxdDDuS9wHHfeYD1h5C2onEHh3D8Vq', // Benjamin Piette
+  'did:elastos:iXyYFboFAd2d9VmfqSvppqg1XQxBtX9ea2', // Trinity Tech
+  'did:elastos:insTmxdDDuS9wHHfeYD1h5C2onEHh3D8Vq', // Benjamin Piette, Trinity Tech
+  'did:elastos:imxNkhKuuXaefyFKQuzFnkfRdedDVLYmKV', // Tuum Tech
+  'did:elastos:iXEgnmYTbFGSsVf16gkVcrGuK3Nv2vGTMD', // Donald Bullers, Tuum Tech
+  'did:elastos:ijQcPGNKAUWQEwoTPTHqLwGeBVS6CidePP', // Michael S, community, moderator
 ];
