069693e2b884fb847e2fc80ca4fa8ffcf09f2609
raozhiming <raozhiming@elastos.org>
2021-07-27 18:52:06 +0800

    CU-18dxwjp wallet: ELA: the confirmation number of the withdrawal transaction is wrong

--- src/app/wallet/pages/wallet/coin/coin-tx-info/coin-tx-info.page.ts
+++ src/app/wallet/pages/wallet/coin/coin-tx-info/coin-tx-info.page.ts
@@ -123,17 +123,17 @@ export class CoinTxInfoPage implements OnInit {
 
             const transaction = await (this.subWallet as MainAndIDChainSubWallet).getTransactionDetails(this.transactionInfo.txid);
             if (transaction) {
+              this.transactionInfo.confirmStatus = transaction.confirmations;
+              // If the fee is too small, then amount doesn't subtract fee
+              // if (transaction.Fee > 10000000000) {
+              //   this.amount = this.amount.minus(this.payFee);
+              // }
+
+              // Tx is ETH - Define amount, fee, total cost and address
               if (this.direction === TransactionDirection.SENT) {
                 // Address: sender address or receiver address
                 this.targetAddress = await (this.subWallet as MainAndIDChainSubWallet).getRealAddressInCrosschainTx(transaction);
-                this.transactionInfo.confirmStatus = transaction.confirmations;
 
-                // If the fee is too small, then amount doesn't subtract fee
-                // if (transaction.Fee > 10000000000) {
-                //   this.amount = this.amount.minus(this.payFee);
-                // }
-
-                // Tx is ETH - Define amount, fee, total cost and address
               } else if (this.direction === TransactionDirection.RECEIVED) {
                 // TODO: show all the inputs and outputs.
               }
