171d2267edaeed1497bb33060bf51b9eccf70cbb
Dongxiao <kuit@msn.com>
2021-08-04 16:06:06 +0800

    #1ax9jn5 fixed: [DPoS Voting] When switching wallets, the last voted node checked on the homepage and the voting history need to be switched

--- src/app/vote/routing.ts
+++ src/app/vote/routing.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SelectWalletPage } from './pages/select-wallet/select-wallet.page';
 
 const routes: Routes = [
-  { path: 'select-wallet', component: SelectWalletPage },
 ];
 
 @NgModule({
