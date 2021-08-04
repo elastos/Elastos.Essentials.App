3b0dcc9a8fd428cebcabe84402fb3fbd3c257686
Dongxiao <kuit@msn.com>
2021-08-04 13:53:13 +0800

    #9w0u16 [CR Proposal] improve create suggestion and proposal's change proposal

--- src/app/crproposalvoting/pages/intents/createsuggestion/createsuggestion.ts
+++ src/app/crproposalvoting/pages/intents/createsuggestion/createsuggestion.ts
@@ -84,7 +84,7 @@ export class CreateSuggestionPage {
             // Fetch more details about this suggestion, to display to the user
             this.suggestionDetails = await this.proposalService.fetchSuggestionDetails(this.suggestionID);
             Logger.log('crproposal', "suggestionDetails", this.suggestionDetails);
-            if (this.proposaltype == "changeproposalowner" && this.suggestionDetails.newAddress) {
+            if (this.proposaltype == "changeproposalowner" && this.suggestionDetails.newAddress && !this.suggestionDetails.newOwnerDID) {
                 this.proposaltype = "changeproposaladdress";
             }
             this.creationDate = Util.timestampToDateTime(this.suggestionDetails.createdAt * 1000);
