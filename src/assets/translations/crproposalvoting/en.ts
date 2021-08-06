24907af2f407839932f98e5db7aeb80f50f9dd41
Dongxiao <kuit@msn.com>
2021-08-06 20:47:45 +0800

    #bv5ygc #bv5xwh fixed: [CR Proposal] Update milestone and Review Milestone bugs

--- src/assets/translations/crproposalvoting/en.ts
+++ src/assets/translations/crproposalvoting/en.ts
@@ -77,6 +77,7 @@ export const en = {
         'vote-result': 'Vote Result',
         'approve': 'Approve',
         'reject': 'Reject',
+        'finalized': 'Finalized',
         // 'abstain': 'Abstain',
 
         'vote-proposal': 'Vote Against',
