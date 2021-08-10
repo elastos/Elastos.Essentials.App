945ddb0f6f2cc40690078d0a7a4c53356ad60041
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2021-08-10 13:55:00 +0800

    CU-cqpz84 BREAKING CHANGE - Replace ionic storage sqlite with indexeddb - DIDs or wallets must be reimported - local data lost

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -228,7 +228,7 @@ export function TranslateLoaderFactory() {
     }),
     IonicStorageModule.forRoot({
       name: '__essentials.db',
-      driverOrder: ['sqlite',  'indexeddb', 'localstorage', 'websql']
+      driverOrder: ['indexeddb', 'sqlite', 'localstorage', 'websql']
     }),
     BrowserAnimationsModule,
   ],
