1cc04ecd8c9934c8b4ac21ba4772c95ca5d1aa12
raozhiming <raozhiming@elastos.org>
2021-08-12 11:22:55 +0800

    fix build error

--- src/app/helpers/sleep.helper.ts
+++ src/app/helpers/sleep.helper.ts
@@ -5,3 +5,13 @@ export async function sleep(ms: number): Promise<void> {
         }, ms);
     })
 }
+
+/**
+ * Executes the given code after a given delay.
+ * This method can't be awaited, this is on purpose, to delay non mandatory actions for later.
+ */
+ export function runDelayed(method: () => unknown, delayMs: number) {
+  setTimeout(() => {
+      method();
+  }, delayMs);
+}
