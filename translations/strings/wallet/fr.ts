3db3a0302be3216db768ef323d34b89b8f8dfa9f
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2021-08-25 11:10:10 +0800

    CU-1dyp74w Coin list - the introduction text contains info about ELA for all networks

--- translations/strings/wallet/fr.ts
+++ translations/strings/wallet/fr.ts
@@ -273,7 +273,7 @@ export const fr = {
         *************************/
         "coin-list-title": "Gestion des Jetons",
         "coin-list-enable-disable-coins": "Activer/Désactiver des Jetons",
-        "coin-list-enable-disable-coins-intro": "Sélectionnez les jetons que vous souhaitez afficher dans le portefeuille actuel. Le jeton principal ELA et les chaînes principales Elastos sont toujours actives.",
+        "coin-list-enable-disable-coins-intro": "Sélectionnez les jetons que vous souhaitez afficher dans le portefeuille actuel.",
         "erc-20-token": "Jeton ERC20",
 
         /***********************
