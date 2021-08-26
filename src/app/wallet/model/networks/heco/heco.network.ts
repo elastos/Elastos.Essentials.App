e331271a4420c28b0147fe40b766b3d4a6ad20b5
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2021-08-26 11:46:33 +0800

    CU-1dyp5eq Wallet home subwallets list is truncated at the bottom

--- src/app/wallet/model/networks/heco/heco.network.ts
+++ src/app/wallet/model/networks/heco/heco.network.ts
@@ -17,11 +17,11 @@ export class HECONetwork extends Network {
     let availableCoins: ERC20Coin[] = [];
 
     if (networkTemplate == MAINNET_TEMPLATE) {
-      availableCoins.push(new ERC20Coin("BTC", "BTC", "Heco-Peg BTC", "0x66a79d23e58475d2738179ca52cd0b41d73f0bea", MAINNET_TEMPLATE, false));
-      availableCoins.push(new ERC20Coin("ETH", "ETH", "Heco-Peg ETH", "0x64ff637fb478863b7468bc97d30a5bf3a428a1fd", MAINNET_TEMPLATE, false));
-      availableCoins.push(new ERC20Coin("USDT", "USDT", "Heco-Peg USDT", "0xa71EdC38d189767582C38A3145b5873052c3e47a", MAINNET_TEMPLATE, false));
-      availableCoins.push(new ERC20Coin("USDC", "USDC", "Heco-Peg USDC", "0x9362bbef4b8313a8aa9f0c9808b80577aa26b73b", MAINNET_TEMPLATE, false));
-      availableCoins.push(new ERC20Coin("DOT", "DOT", "Heco-Peg DOT", "0xa2c49cee16a5e5bdefde931107dc1fae9f7773e3", MAINNET_TEMPLATE, false));
+      availableCoins.push(new ERC20Coin("BTC", "BTC", "Heco BTC", "0x66a79d23e58475d2738179ca52cd0b41d73f0bea", MAINNET_TEMPLATE, false));
+      availableCoins.push(new ERC20Coin("ETH", "ETH", "Heco ETH", "0x64ff637fb478863b7468bc97d30a5bf3a428a1fd", MAINNET_TEMPLATE, false));
+      availableCoins.push(new ERC20Coin("USDT", "USDT", "Heco USDT", "0xa71EdC38d189767582C38A3145b5873052c3e47a", MAINNET_TEMPLATE, false));
+      availableCoins.push(new ERC20Coin("USDC", "USDC", "Heco USDC", "0x9362bbef4b8313a8aa9f0c9808b80577aa26b73b", MAINNET_TEMPLATE, false));
+      availableCoins.push(new ERC20Coin("DOT", "DOT", "Heco DOT", "0xa2c49cee16a5e5bdefde931107dc1fae9f7773e3", MAINNET_TEMPLATE, false));
     }
 
     return availableCoins;
