ab56054f9f8b4503927e0f71647fe6ed2a89e59c
raozhiming <raozhiming@elastos.org>
2021-08-26 17:16:51 +0800

    CU-ebcxv1 [wallet]Show transaction list for heco

--- src/app/wallet/model/wallets/heco/heco.erc20.subwallet.ts
+++ src/app/wallet/model/wallets/heco/heco.erc20.subwallet.ts
@@ -1,4 +1,7 @@
+import { Logger } from "src/app/logger";
+import { GlobalJsonRPCService } from "src/app/services/global.jsonrpc.service";
 import { CoinID, StandardCoinName } from "../../Coin";
+import { EthTransaction } from "../../evm.types";
 import { ERC20SubWallet } from "../erc20.subwallet";
 import { NetworkWallet } from "../networkwallet";
 import { HecoAPI, HecoApiType } from "./heco.api";
@@ -26,6 +29,33 @@ export class HecoERC20SubWallet extends ERC20SubWallet {
   }
 
   protected async getTransactionsByRpc() {
-    // Do nothing for now - Not implemented / Not sure we can get this for heco.
+    const contractAddress = this.coin.getContractAddress().toLowerCase();
+    const tokenAccountAddress = await this.getTokenAccountAddress();
+    let result = await this.getHECOTransactions(this.elastosChainCode, tokenAccountAddress);
+    if (result) {
+      let allTx = result.filter((tx)=> {
+        return tx.to === contractAddress || tx.contractAddress === contractAddress
+      })
+      this.transactions = {totalcount:allTx.length, txhistory:allTx.reverse()};
+      // TODO:Parse the input data.
+
+      await this.saveTransactions(this.transactions.txhistory as EthTransaction[]);
+    }
   }
+
+  private async getHECOTransactions(chainID: StandardCoinName, address: string, begBlockNumber = 0, endBlockNumber = 0): Promise<EthTransaction[]> {
+    const rpcApiUrl = HecoAPI.getApiUrl(HecoApiType.ACCOUNT_RPC);
+    if (rpcApiUrl === null) {
+      return null;
+    }
+    let hecoTxlistUrl = rpcApiUrl + '/api?module=account&action=txlist&address=' + address;
+    try {
+      let result = await GlobalJsonRPCService.instance.httpGet(hecoTxlistUrl);
+      return result.result as EthTransaction[];
+    } catch (e) {
+      Logger.error('wallet', 'getHECOTransactions error:', e)
+    }
+    return null;
+  }
+
 }
