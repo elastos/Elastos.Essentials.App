7ccea9e4d74f305d855fec579ce9b286f70a3ddb
raozhiming <raozhiming@elastos.org>
2021-08-27 11:51:20 +0800

    CU-1e4ermd [Wallet] Auto discover and add HRC20 tokens

--- src/app/wallet/model/wallets/heco/heco.networkwallet.ts
+++ src/app/wallet/model/wallets/heco/heco.networkwallet.ts
@@ -4,7 +4,10 @@ import { HECOChainSubWallet } from "./heco.subwallet";
 import { StandardCoinName } from "../../Coin";
 import { Network } from "../../networks/network";
 import { StandardEVMSubWallet } from "../evm.subwallet";
-import { ERC20TokenInfo } from "../../evm.types";
+import { ERC20TokenInfo, EthTokenTransaction } from "../../evm.types";
+import { HecoAPI, HecoApiType } from "./heco.api";
+import { GlobalJsonRPCService } from "src/app/services/global.jsonrpc.service";
+import { Logger } from "src/app/logger";
 
 export class HecoNetworkWallet extends NetworkWallet {
   private mainTokenSubWallet: HECOChainSubWallet = null;
@@ -28,6 +31,49 @@ export class HecoNetworkWallet extends NetworkWallet {
   }
 
   public async getERCTokensList(): Promise<ERC20TokenInfo[]> {
-    return await []; // Not yet implemented
+    // It will cost some time if there are lot of transactions.
+    let tokenTransactions = await this.getHRC20TokenTransferEvents(0);
+    return this.getERCTokensFromTransferEvents(tokenTransactions);
+  }
+
+  /**
+   * Can not get the token list directly, So get the token list by token transfer events.
+   */
+  private getERCTokensFromTransferEvents(transferEvents: EthTokenTransaction[]) {
+    let ercTokens: ERC20TokenInfo[] = [];
+    let ercTokenSymbols = [];
+    for (let i = 0, len = transferEvents.length; i < len; i++) {
+      if (-1 === ercTokenSymbols.indexOf(transferEvents[i].tokenSymbol)) {
+        ercTokenSymbols.push(transferEvents[i].tokenSymbol);
+        let token: ERC20TokenInfo = {
+          balance: '',
+          contractAddress: transferEvents[i].contractAddress,
+          decimals: transferEvents[i].tokenDecimal,
+          name: transferEvents[i].tokenName,
+          symbol: transferEvents[i].tokenSymbol,
+          type : 'ERC-20',
+        }
+        ercTokens.push(token);
+      }
+    }
+    Logger.log('wallet', ' HRC20 Tokens:', ercTokens)
+    return ercTokens;
+  }
+
+  private async getHRC20TokenTransferEvents(startblock:number, endblock = 9999999999): Promise<EthTokenTransaction[]> {
+    let tokenSubWallet = this.getMainEvmSubWallet();
+    const address = await tokenSubWallet.getTokenAddress();
+    const rpcApiUrl = HecoAPI.getApiUrl(HecoApiType.ACCOUNT_RPC);
+    if (rpcApiUrl === null) {
+      return null;
+    }
+    let hecoTokensEventUrl = rpcApiUrl + '/api?module=account&action=tokentx&address=' + address
+          + '&startblock=' + startblock + '&endblock=' + endblock;
+    try {
+      let result = await GlobalJsonRPCService.instance.httpGet(hecoTokensEventUrl);
+      return result.result as EthTokenTransaction[];
+    } catch (e) {
+      Logger.error('wallet', 'getHRC20TokenTransferEvents error:', e)
+    }
   }
 }
