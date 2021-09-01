18a929c06a92ae8c69cb91d0b08bae9bd55b32ae
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2021-09-01 17:08:21 +0800

    CU-1enftdf WIP - Transaction providers - factorized some code into the main provider class

--- src/app/wallet/model/wallets/elastos/providers/elastos.transaction.provider.ts
+++ src/app/wallet/model/wallets/elastos/providers/elastos.transaction.provider.ts
@@ -1,24 +1,13 @@
-import { Subject } from "rxjs";
-import { Logger } from "src/app/logger";
-import { JSONObject } from "src/app/model/json";
-import { GlobalElastosAPIService } from "src/app/services/global.elastosapi.service";
-import { GlobalJsonRPCService } from "src/app/services/global.jsonrpc.service";
-import { StandardCoinName, TokenAddress } from "../../../Coin";
-import { EthTransaction } from "../../../evm.types";
-import { TimeBasedPersistentCache } from "../../../timebasedpersistentcache";
-import { ElastosPaginatedTransactions, ElastosTransaction, GenericTransaction, PaginatedTransactions, TransactionDirection, TransactionStatus } from "../../../transaction.types";
-import { NewToken, NewTransaction, TransactionProvider, SubWalletTransactionProvider, AnySubWalletTransactionProvider } from "../../../transaction.provider";
+import { StandardCoinName, } from "../../../Coin";
+import { ElastosTransaction, GenericTransaction } from "../../../transaction.types";
+import { TransactionProvider, AnySubWalletTransactionProvider } from "../../../transaction.provider";
 import { NetworkWallet } from "../../networkwallet";
 import { AnySubWallet, SubWallet } from "../../subwallet";
 import { EidSubWallet } from "../eid.evm.subwallet";
 import { EscSubWallet } from "../esc.evm.subwallet";
 import { IDChainSubWallet } from "../idchain.subwallet";
 import { MainchainSubWallet } from "../mainchain.subwallet";
-import { StandardSubWallet } from "../../standard.subwallet";
-import { StandardEVMSubWallet } from "../../evm.subwallet";
-import { ElastosAPI } from "../elastos.api";
 import { ElastosERC20SubWallet } from "../elastos.erc20.subwallet";
-import { WalletHelper } from "../wallet.helper";
 import { MainchainProvider } from "./mainchain.transaction.provider";
 import { EscProvider } from "./esc.transaction.provider";
 import { TokenProvider } from "./token.transaction.provider";
@@ -57,27 +46,11 @@ export class ElastosTransactionProvider extends TransactionProvider<ElastosTrans
     this.tokenProvider = new TokenProvider(this, this.escSubWallet);
     await this.tokenProvider.initialize();
 
-    void this.refreshAll();
-  }
-
-  private rearmRefreshTimeout() {
-    setTimeout(() => {
-      void this.refreshAll();
-    }, 30000); // 30s
-  }
-
-  private async refreshAll(): Promise<void> {
-    await Promise.all([
-      this.mainChainProvider.fetchTransactions(),
-      // TODO this.idChainProvider.fetchTransactions(),
-      this.escProvider.fetchTransactions(),
-      this.eidProvider.fetchTransactions(),
-      this.tokenProvider.fetchTransactions(),
-    ]);
-
-    // Only restart a timer after all current operations are complete. We don't want to use an internal
-    // that would create many slow updates in parrallel.
-    this.rearmRefreshTimeout();
+    this.refreshEvery(() => this.mainChainProvider.fetchTransactions(), 30000);
+    // TODO this.idChainProvider.fetchTransactions(),
+    this.refreshEvery(() => this.escProvider.fetchTransactions(), 30000);
+    this.refreshEvery(() => this.eidProvider.fetchTransactions(), 30000);
+    this.refreshEvery(() => this.tokenProvider.fetchTransactions(), 30000);
   }
 
   public stop(): Promise<void> {
@@ -85,28 +58,16 @@ export class ElastosTransactionProvider extends TransactionProvider<ElastosTrans
     return;
   }
 
-  public getTransactions(subWallet: SubWallet<GenericTransaction>, startIndex = 0): ElastosTransaction[] {
-    return this.getSubWalletTransactionProvider(subWallet).getTransactions(subWallet, startIndex);
-  }
-
-  public canFetchMoreTransactions(subWallet: AnySubWallet): boolean {
-    return this.getSubWalletTransactionProvider(subWallet).canFetchMoreTransactions(subWallet);
-  }
-
-  public forcedFetchTransactions(subWallet: AnySubWallet, afterTransaction?: GenericTransaction) {
-    return this.getSubWalletTransactionProvider(subWallet).forcedFetchTransactions(subWallet, afterTransaction);
-  }
-
-  public prepareTransactions(subWallet: AnySubWallet): Promise<void> {
-    return this.getSubWalletTransactionProvider(subWallet).prepareTransactions(subWallet);
-  }
-
-  private getSubWalletTransactionProvider(subWallet: AnySubWallet): AnySubWalletTransactionProvider {
-    if (subWallet instanceof MainchainSubWallet) return this.mainChainProvider;
-    else if (subWallet instanceof EscSubWallet) return this.escProvider;
-    else if (subWallet instanceof EidSubWallet) return this.eidProvider;
-    else if (subWallet instanceof ElastosERC20SubWallet) return this.tokenProvider;
+  protected getSubWalletTransactionProvider(subWallet: AnySubWallet): AnySubWalletTransactionProvider {
+    if (subWallet instanceof MainchainSubWallet)
+      return this.mainChainProvider;
+    else if (subWallet instanceof EscSubWallet)
+      return this.escProvider;
+    else if (subWallet instanceof EidSubWallet)
+      return this.eidProvider;
+    else if (subWallet instanceof ElastosERC20SubWallet)
+      return this.tokenProvider;
     else
-      throw new Error("getTransactions for UNKNOWN SUBWALLET: NOT IMPLEMENTED");
+      throw new Error("Elastos transactions provider: getSubWalletTransactionProvider() is called with an unknown subwallet!");
   }
 }
