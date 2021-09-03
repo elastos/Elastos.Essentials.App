bacc35d83771c6f4701474f0fdf75d260fe93b00
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2021-09-03 09:24:05 +0800

    CU-1fce88m Implement the Elastos IDChain provider

--- src/app/wallet/model/wallets/elastos/elastos.api.ts
+++
@@ -1,9 +0,0 @@
-import { Logger } from "src/app/logger";
-import { GlobalElastosAPIService } from "src/app/services/global.elastosapi.service";
-import { StandardCoinName } from "../../Coin";
-import { PaginatedTransactions, ElastosTransaction } from "../../providers/transaction.types";
-import { SubWallet } from "../subwallet";
-import { EscSubWallet } from "./esc.evm.subwallet";
-
-export class ElastosAPI {
-}
