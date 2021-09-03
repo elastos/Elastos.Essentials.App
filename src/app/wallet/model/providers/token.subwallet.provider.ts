0eed68d41ad5f732b72dac81211fb37a781cdcf3
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2021-09-03 08:41:49 +0800

    CU-gacybn [Wallet] Implement pagination and canFetchMore for the default token EVM provider

--- src/app/wallet/model/providers/token.subwallet.provider.ts
+++ src/app/wallet/model/providers/token.subwallet.provider.ts
@@ -8,11 +8,15 @@ import { ProviderTransactionInfo } from "./providertransactioninfo";
 import { TransactionProvider } from "./transaction.provider";
 import { SubWalletTransactionProvider } from "./subwallet.provider";
 
+const MAX_RESULTS_PER_FETCH = 8; // TODO: increase after dev complete
+
 /**
  * Root class for all EVM compatible chains to handle ERC20 tokens, as they use the same endpoints to get the list
  * of transactions.
  */
 export abstract class EVMSubWalletTokenProvider<SubWalletType extends StandardEVMSubWallet<EthTransaction>> extends SubWalletTransactionProvider<SubWalletType, EthTransaction> {
+  private canFetchMore = true;
+
   constructor(provider: TransactionProvider<any>, subWallet: SubWalletType, protected rpcApiUrl: string) {
     super(provider, subWallet);
   }
@@ -27,25 +31,43 @@ export abstract class EVMSubWalletTokenProvider<SubWalletType extends StandardEV
   }
 
   public canFetchMoreTransactions(subWallet: AnySubWallet): boolean {
-    console.log("DEBUG token canFetchMoreTransactions");
-    return true; // TMP
+    return this.canFetchMore;
   }
 
   public async fetchTransactions(erc20SubWallet: ERC20SubWallet, afterTransaction?: EthTransaction): Promise<void> {
-    if (afterTransaction)
-      throw new Error("token fetchTransactions() with afterTransaction: NOT YET IMPLEMENTED");
-
     await this.prepareTransactions(this.subWallet);
 
-    console.log("DEBUG token provider fetchTransactions");
+    let page = 1;
+    // Compute the page to fetch from the api, based on the current position of "afterTransaction" in the list
+    if (afterTransaction) {
+      let afterTransactionIndex = this.getTransactions(this.subWallet).findIndex(t => t.hash === afterTransaction.hash);
+      if (afterTransactionIndex) { // Just in case, should always be true but...
+        // Ex: if tx index in current list of transactions is 18 and we use 8 results per page
+        // then the page to fetch is 2: Math.floor(18 / 8) + 1 - API page index starts at 1
+        page = 1 + Math.floor((afterTransactionIndex+1) / MAX_RESULTS_PER_FETCH);
+      }
+    }
 
     const contractAddress = erc20SubWallet.coin.getContractAddress().toLowerCase();
     const accountAddress = await this.subWallet.createAddress();
-    let txListUrl = this.rpcApiUrl + '/api?module=account&action=txlist&contractaddress=' + contractAddress
-            + '&address=' + accountAddress + '&sort=desc';
+    let txListUrl = this.rpcApiUrl + '/api?module=account';
+    txListUrl += '&action=txlist';
+    txListUrl += '&page='+page;
+    txListUrl += '&offset='+MAX_RESULTS_PER_FETCH;
+    txListUrl += '&sort=desc';
+    txListUrl += '&contractaddress=' + contractAddress;
+    txListUrl += '&address=' + accountAddress;
+
     try {
       let result = await GlobalJsonRPCService.instance.httpGet(txListUrl);
       let transactions = result.result as EthTransaction[];
+
+      if (transactions.length < MAX_RESULTS_PER_FETCH) {
+        // Got less results than expected: we are at the end of what we can fetch. remember this
+        // (in memory only)
+        this.canFetchMore = false;
+      }
+
       await this.saveTransactions(transactions);
     } catch (e) {
       Logger.error('wallet', 'EVMSubWalletTokenProvider fetchTransactions error:', e)
