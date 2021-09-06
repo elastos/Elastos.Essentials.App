8899b153a6c1d2aec84192b375abb370a02227b0
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2021-09-06 15:57:37 +0800

    CU-1befpj4 Fusion network crashes completed

--- src/app/wallet/model/wallets/fusion/providers/token.subwallet.provider.ts
+++ src/app/wallet/model/wallets/fusion/providers/token.subwallet.provider.ts
@@ -1,20 +1,28 @@
+import { Logger } from "src/app/logger";
 import { EthTransaction } from "../../../evm.types";
 import { ProviderTransactionInfo } from "../../../providers/providertransactioninfo";
-import { SubWalletTransactionProvider } from "../../../providers/subwallet.provider";
-import { GenericTransaction, TransactionDirection } from "../../../providers/transaction.types";
-import { EscSubWallet } from "../../elastos/esc.evm.subwallet";
+import { EVMSubWalletTokenProvider } from "../../../providers/token.subwallet.provider";
+import { TransactionDirection } from "../../../providers/transaction.types";
+import { ERC20SubWallet } from "../../erc20.subwallet";
+import { StandardEVMSubWallet } from "../../evm.subwallet";
 import { AnySubWallet } from "../../subwallet";
 
-export class FusionTokenSubWalletProvider extends SubWalletTransactionProvider<EscSubWallet, EthTransaction> {
+export class FusionTokenSubWalletProvider extends EVMSubWalletTokenProvider<StandardEVMSubWallet> {
   protected getProviderTransactionInfo(transaction: { blockHash: string; blockNumber: string; confirmations: string; contractAddress: string; cumulativeGasUsed: string; from: string; gas: string; gasPrice: string; gasUsed: string; hash: string; input: string; isError: string; nonce: string; timeStamp: string; to: string; transactionIndex: string; txreceipt_status: string; value: string; Direction: TransactionDirection; isERC20TokenTransfer: boolean; }): ProviderTransactionInfo {
-    throw new Error("Method not implemented.");
+    throw new Error("Fusion - getProviderTransactionInfo(): Method not implemented.");
   }
 
   public canFetchMoreTransactions(subWallet: AnySubWallet): boolean {
-    throw new Error("Method not implemented.");
+    return false;
   }
 
-  public fetchTransactions(subWallet: AnySubWallet, afterTransaction?: GenericTransaction) {
-    throw new Error("Method not implemented.");
+  public fetchTransactions(erc20SubWallet: ERC20SubWallet, afterTransaction?: EthTransaction): Promise<void> {
+    Logger.log("wallet", "Fusion: fetchTransactions() not implemented");
+    return;
+  }
+
+  public fetchAllTokensTransactions(): Promise<void> {
+    Logger.log("wallet", "Fusion: fetchAllTokensTransactions() not implemented");
+    return;
   }
 }
