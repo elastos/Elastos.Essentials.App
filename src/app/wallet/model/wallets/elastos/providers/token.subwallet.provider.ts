1af16968f40d359d8b8764ac5c233048ae37dfff
raozhiming <raozhiming@elastos.org>
2021-09-07 18:18:53 +0800

    CU-1fce86m [Wallet] Implement pagination for ERC20 providers

--- src/app/wallet/model/wallets/elastos/providers/token.subwallet.provider.ts
+++ src/app/wallet/model/wallets/elastos/providers/token.subwallet.provider.ts
@@ -1,12 +1,19 @@
+import { Logger } from "src/app/logger";
 import { GlobalElastosAPIService } from "src/app/services/global.elastosapi.service";
+import { GlobalJsonRPCService } from "src/app/services/global.jsonrpc.service";
 import { StandardCoinName } from "../../../Coin";
 import { EthTransaction } from "../../../evm.types";
 import { ProviderTransactionInfo } from "../../../providers/providertransactioninfo";
 import { SubWalletTransactionProvider } from "../../../providers/subwallet.provider";
+import { ERC20SubWallet } from "../../erc20.subwallet";
 import { AnySubWallet } from "../../subwallet";
 import { EscSubWallet } from "../esc.evm.subwallet";
 
+const MAX_RESULTS_PER_FETCH = 100; // TODO: increase after dev complete
+
 export class ElastosTokenSubWalletProvider extends SubWalletTransactionProvider<EscSubWallet, EthTransaction> {
+  private canFetchMore = false;
+
   protected getProviderTransactionInfo(transaction: EthTransaction): ProviderTransactionInfo {
     return {
       cacheKey: this.subWallet.masterWallet.id + "-" + this.subWallet.networkWallet.network.key + "-" + transaction.contractAddress.toLowerCase() + "-transactions",
@@ -29,19 +36,47 @@ export class ElastosTokenSubWalletProvider extends SubWalletTransactionProvider<
   }
 
   public canFetchMoreTransactions(subWallet: AnySubWallet): boolean {
-    return true; // TODO
+    return this.canFetchMore;
   }
 
-  // All ERC20 transactions fetched at once
-  public async fetchTransactions(subWallet: AnySubWallet, afterTransaction?: EthTransaction): Promise<void> {
-    if (afterTransaction)
-      throw new Error("fetchTransactions() with afterTransaction: NOT YET IMPLEMENTED");
+  public async fetchTransactions(erc20SubWallet: ERC20SubWallet, afterTransaction?: EthTransaction): Promise<void> {
+    let page = 1;
+    // Compute the page to fetch from the api, based on the current position of "afterTransaction" in the list
+    if (afterTransaction) {
+      let afterTransactionIndex = (await this.getTransactions(this.subWallet)).findIndex(t => t.hash === afterTransaction.hash);
+      if (afterTransactionIndex) { // Just in case, should always be true but...
+        // Ex: if tx index in current list of transactions is 18 and we use 8 results per page
+        // then the page to fetch is 2: Math.floor(18 / 8) + 1 - API page index starts at 1
+        page = 1 + Math.floor((afterTransactionIndex + 1) / MAX_RESULTS_PER_FETCH);
+      }
+    }
 
-    const tokenAccountAddress = await this.subWallet.createAddress();
+    let apiurltype = GlobalElastosAPIService.instance.getApiUrlTypeForBrowser(this.subWallet.id);
+    const accountApiUrl = GlobalElastosAPIService.instance.getApiUrl(apiurltype);
 
-    let ethTokenTransactions = await GlobalElastosAPIService.instance.getERC20TokenTransactions(StandardCoinName.ETHSC, tokenAccountAddress);
-    if (ethTokenTransactions) {
-      await this.saveTransactions(ethTokenTransactions);
+    const contractAddress = erc20SubWallet.coin.getContractAddress().toLowerCase();
+    const accountAddress = await this.subWallet.createAddress();
+    let txListUrl = accountApiUrl + '/api?module=account';
+    txListUrl += '&action=tokentx';
+    txListUrl += '&page=' + page;
+    txListUrl += '&offset=' + MAX_RESULTS_PER_FETCH;
+    txListUrl += '&sort=desc';
+    txListUrl += '&contractaddress=' + contractAddress;
+    txListUrl += '&address=' + accountAddress;
+
+    try {
+      let result = await GlobalJsonRPCService.instance.httpGet(txListUrl);
+      let transactions = result.result as EthTransaction[];
+
+      if (transactions.length < MAX_RESULTS_PER_FETCH) {
+        // Got less results than expected: we are at the end of what we can fetch. remember this
+        // (in memory only)
+        this.canFetchMore = false;
+      }
+
+      await this.saveTransactions(transactions);
+    } catch (e) {
+      Logger.error('wallet', 'ElastosTokenSubWalletProvider fetchTransactions error:', e)
     }
   }
 }
