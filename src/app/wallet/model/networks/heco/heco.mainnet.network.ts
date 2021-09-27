9bdf24bcc4e0b1a9a4b092fc1a40a8310908fcbc
raozhiming <raozhiming@elastos.org>
2021-09-27 10:55:06 +0800

    CU-1jkdcyy [Wallet] Add the new hELA as default visible Heco tokens

--- src/app/wallet/model/networks/heco/heco.mainnet.network.ts
+++ src/app/wallet/model/networks/heco/heco.mainnet.network.ts
@@ -17,11 +17,12 @@ export class HECOMainNetNetwork extends EVMNetwork {
       128,
       [
         new ERC20Coin("BTC", "Heco BTC", "0x66a79d23e58475d2738179ca52cd0b41d73f0bea", MAINNET_TEMPLATE, false, true),
+        new ERC20Coin("ELA", "Heco ELA", "0x102A56E6c2452bcee99dF8f61167E3e0f0749dbE", MAINNET_TEMPLATE, false, true),
         new ERC20Coin("ETH", "Heco ETH", "0x64ff637fb478863b7468bc97d30a5bf3a428a1fd", MAINNET_TEMPLATE, false, true),
+        new ERC20Coin("FilDA", "FilDA", "0xe36ffd17b2661eb57144ceaef942d95295e637f0", MAINNET_TEMPLATE, false, true),
         new ERC20Coin("USDT", "Heco USDT", "0xa71EdC38d189767582C38A3145b5873052c3e47a", MAINNET_TEMPLATE, false, true),
         new ERC20Coin("USDC", "Heco USDC", "0x9362bbef4b8313a8aa9f0c9808b80577aa26b73b", MAINNET_TEMPLATE, false, true),
         new ERC20Coin("DOT", "Heco DOT", "0xa2c49cee16a5e5bdefde931107dc1fae9f7773e3", MAINNET_TEMPLATE, false),
-        new ERC20Coin("FilDA", "FilDA", "0xe36ffd17b2661eb57144ceaef942d95295e637f0", MAINNET_TEMPLATE, false, true)
       ]
     );
   }
