07a46cce843edd89c6ba4ccab9f96a9f56bd1e2d
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2021-09-28 14:07:03 +0800

    CU-1jkg5fd [Heco, BSC] Add swap tokens for mdex.com

--- src/app/wallet/services/swap.service.ts
+++ src/app/wallet/services/swap.service.ts
@@ -81,8 +81,53 @@ export class SwapService implements InAppBrowserClient {
                 name: "MDEX",
                 projectUrl: "https://www.mdex.co",
                 swapUrl: "https://ht.mdex.co/#/swap?inputCurrency=${inputCurrency}",
+                // TODO: HT
                 swappableTokenContracts: [
-                    "0x9362bbef4b8313a8aa9f0c9808b80577aa26b73b" // hUSDC
+                    "0x25d2e80cb6b86881fd7e07dd263fb79f4abe033c", // MDX
+                    "0x22c54ce8321a4015740ee1109d9cbc25815c46e6", // UNI
+                    "0x0298c2b32eae4da002a15f36fdf7615bea3da047", // HUSD
+                    "0xa71edc38d189767582c38a3145b5873052c3e47a", // USDT
+                    "0x843af718ef25708765a8e0942f89edeae1d88df0", // ADA
+                    "0x3d760a45d0887dfd89a2f5385a236b29cb46ed2a", // DAI
+                    "0x40280E26A572745B1152A54D1D44F365DaA51618", // DOGE
+                    "0xef3cebd77e0c52cb6f60875d9306397b5caca375", // HBCH
+                    "0xa2c49cee16a5e5bdefde931107dc1fae9f7773e3", // HDOT
+                    "0xae3a768f9ab104c69a7cd6041fe16ffa235d1810", // HFIL
+                    "0xecb56cf772b5c9a6907fb7d32387da2fcbfb63b4", // HLTC
+                    "0x9e004545c59d359f6b7bfb06a26390b087717b42", // LINK
+                    "0x9362bbef4b8313a8aa9f0c9808b80577aa26b73b", // USDC
+                    "0x5545153ccfca01fbd7dd11c0b23ba694d9509a6f", // WHT
+                    "0xa2f3c2446a3e20049708838a779ff8782ce6645a", // XRP
+                ]
+            },
+        ],
+        "bsc": [
+            {
+                logo: "/assets/wallet/earn/mdex.png",
+                name: "MDEX",
+                projectUrl: "https://bsc.mdex.co/",
+                swapUrl: "https://bsc.mdex.co/#/swap?inputCurrency=${inputCurrency}",
+                // TODO: BNB
+                swappableTokenContracts: [
+                    "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD
+                    "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c", // BTCB
+                    "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
+                    "0x2170ed0880ac9a755fd29b2688956bd959f933f8", // ETH
+                    "0x3ee2200efb3400fabb9aacf31297cbdd1d435d47", // ADA
+                    "0x8ff795a6f4d97e7887c79bea79aba5cc76444adf", // BCH
+                    "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82", // CAKE
+                    "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3", // DAI
+                    "0xba2ae424d960c26247dd6c32edc70b295c744c43", // DOGE
+                    "0x7083609fce4d1d8dc0c979aab8c869ea2c873402", // DOT
+                    "0x0D8Ce2A99Bb6e3B7Db580eD848240e4a0F9aE153", // FIL
+                    "0x1d2f0da169ceb9fc7b3144628db156f3f6c60dbe", // XRP
+                    "0xf8a0bf9cf54bb92f17374d9e9a321e6a111a51bd", // LINK
+                    "0x9C65AB58d8d978DB963e63f2bfB7121627e3a739", // MDX
+                    "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82", // CAKE
+                    "0xbf5140a22578168fd562dccf235e5d43a02ce9b1", // UNI
+                    "0x55d398326f99059ff775485246999027b3197955", // USDT
+                    "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", // WBNB
+                    "0x4338665cbb7b2485a8855a139b75d5e34ab0db94", // LTC
                 ]
             },
         ]
