131fcbbc99f8eef975feefb81dbfb536edd1f3f3
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2021-10-26 11:16:36 +0800

    CU-1pjpbt9 Missing token valuation for polygon

---
+++ src/app/wallet/model/networks/polygon/currency/polygon.uniswap.currency.provider.ts
@@ -0,0 +1,24 @@
+import { MAINNET_TEMPLATE } from "src/app/services/global.networks.service";
+import { ERC20Coin } from "../../../coin";
+import { UniswapCurrencyProvider } from "../../uniswap.currencyprovider";
+
+export class PolygonMainnetUniswapCurrencyProvider extends UniswapCurrencyProvider {
+  private referenceUSDCoin = new ERC20Coin("USDT", "USDT", "0xc2132d05d31c914a87c6611c10748aeb04b58e8f", 6, MAINNET_TEMPLATE, false, true);
+  private wrappedNativeCoin = new ERC20Coin("WMATIC", "Wrapped MATIC", "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270", 18, MAINNET_TEMPLATE, false, true);
+
+  public getFactoryAddress(): string {
+    return "0xc35DADB65012eC5796536bD9864eD8773aBc74C4"; // Sushiswap factory
+  }
+
+  public getFactoryInitCodeHash(): string {
+    return "0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303"; // Sushiswap factory init code hash
+  }
+
+  public getReferenceUSDCoin(): ERC20Coin {
+    return this.referenceUSDCoin;
+  }
+
+  public getWrappedNativeCoin(): ERC20Coin {
+    return this.wrappedNativeCoin;
+  }
+}
