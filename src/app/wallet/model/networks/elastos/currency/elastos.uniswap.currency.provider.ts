5f5265b13676eb6ae3f791c1f88418eae4ecd6f2
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2021-11-01 15:22:38 +0800

    CU-1phqktv Update glide factory and init codes

--- src/app/wallet/model/networks/elastos/currency/elastos.uniswap.currency.provider.ts
+++ src/app/wallet/model/networks/elastos/currency/elastos.uniswap.currency.provider.ts
@@ -7,11 +7,11 @@ export class ElastosMainnetUniswapCurrencyProvider extends UniswapCurrencyProvid
   private wrappedNativeCoin = new ERC20Coin("WELA", "Wrapped ELA", "0x517e9e5d46c1ea8ab6f78677d6114ef47f71f6c4", 18, MAINNET_TEMPLATE, false, true);
 
   public getFactoryAddress(): string {
-    return "0xad52836ae15ba314915035b22adf31a8a47d0626"; // Glide Finance factory
+    return "0xaAbe38153b25f0d4b2bDa620f67059B3a45334e5"; // Glide Finance factory
   }
 
   public getFactoryInitCodeHash(): string {
-    return "0x0c48b1f41153fe6d5b587df2b75975db915806865521ab360cd34590c69de66a"; // Glide Finance factory init code hash
+    return "0xec67b2700fcbc06d227dbd9b65a9fe8962750aaf96eadc6caf4b94d8d60c1da5"; // Glide Finance factory init code hash
   }
 
   public getReferenceUSDCoin(): ERC20Coin {
