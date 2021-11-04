2ca6421769f6e0335c26edebad0e69da63a54554
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2021-11-04 12:19:31 +0800

    CU-1qdzdqu [Swap] Add ELK Finance as swap and earn providers + Swap service can fetch remote tokens list jsons

--- src/app/wallet/model/networks/arbitrum/arbitrum.mainnet.network.ts
+++ src/app/wallet/model/networks/arbitrum/arbitrum.mainnet.network.ts
@@ -33,6 +33,7 @@ export class ArbitrumMainNetNetwork extends EVMNetwork {
         arbitrumMainnetElkEarnProvider
       ],
       [
+        arbitrumMainnetUniswapSwapProvider,
         arbitrumMainnetUniswapSwapProvider
       ],
       [
