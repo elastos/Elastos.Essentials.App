aa1cd67d3e600e83843026b461d5d88efcaa6eb3
Dongxiao <kuit@msn.com>
2021-11-05 11:19:19 +0800

    #1r0n4vu [CR Council] Improve cr member's avatar and codes

--- src/app/crcouncilvoting/services/candidates.service.ts
+++ src/app/crcouncilvoting/services/candidates.service.ts
@@ -1,30 +1,46 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Candidate } from '../model/candidates.model';
 import { AlertController, ToastController } from '@ionic/angular';
-import { Selected } from '../model/selected.model';
-import { CouncilMember } from '../model/council.model';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 import { Logger } from 'src/app/logger';
+import { App } from 'src/app/model/app.enum';
+import { DIDDocument } from 'src/app/model/did/diddocument.model';
+import { Util } from 'src/app/model/util';
+import { GlobalDIDSessionsService } from 'src/app/services/global.didsessions.service';
+import { ElastosApiUrlType, GlobalElastosAPIService } from 'src/app/services/global.elastosapi.service';
+import { GlobalJsonRPCService } from 'src/app/services/global.jsonrpc.service';
 import { GlobalNavService } from 'src/app/services/global.nav.service';
 import { GlobalStorageService } from 'src/app/services/global.storage.service';
-import { GlobalDIDSessionsService } from 'src/app/services/global.didsessions.service';
-import { Subscription } from 'rxjs';
-import { TranslateService } from '@ngx-translate/core';
-import { DIDDocument } from 'src/app/model/did/diddocument.model';
-import { ElastosApiUrlType, GlobalElastosAPIService } from 'src/app/services/global.elastosapi.service';
-import { App } from 'src/app/model/app.enum';
-import { Util } from 'src/app/model/util';
-import { GlobalJsonRPCService } from 'src/app/services/global.jsonrpc.service';
+import { Candidate } from '../model/candidates.model';
+import { Selected } from '../model/selected.model';
 
+export type CRMemberInfo = {
+    address: string,
+    avatar: string,
+    cid: string,
+    depositAmount: "0"
+    did: string,
+    didName: string,
+    dpospublickey: string,
+    impeachmentRatio: number,
+    impeachmentThroughVotes: number,
+    impeachmentVotes: number,
+    isSelf: boolean,
+    location: number,
+    status: string,
+    term: any[],
+    type: string,
+}
 @Injectable({
     providedIn: 'root'
 })
 export class CandidatesService {
 
     private elaRpcApi: string;
+    private crRpcApi: string;
 
     constructor(
-        private http: HttpClient,
         private globalNav: GlobalNavService,
         private alertCtrl: AlertController,
         private toastCtrl: ToastController,
@@ -33,21 +49,26 @@ export class CandidatesService {
         public jsonRPCService: GlobalJsonRPCService,
         private globalElastosAPIService: GlobalElastosAPIService
     ) {
-        this.elaRpcApi = this.globalElastosAPIService.getApiUrl(ElastosApiUrlType.ELA_RPC);
-    }
 
+    }
 
     /** Election **/
     public candidates: Candidate[] = [];
     public totalVotes = 0;
     public selectedCandidates: Selected[] = [];
     public crmembers: any[] = [];
+    public selectedMember: CRMemberInfo;
 
     /** Election Results **/
-    public councilTerm: number;
+    public councilTerm: any[] = [];
+    public currentTermIndex: number = -1;
+    public votingTermIndex: number = -1;
+
     // public council: CouncilMember[] = [];
 
     private subscription: Subscription = null;
+
+    public isCRMember = false;
 
     public httpOptions = {
         headers: new HttpHeaders({
@@ -55,14 +76,33 @@ export class CandidatesService {
         })
     };
 
+    public avatarList = {
+        "ioNZHmG9CpDvjEfpRNWU1vd8i1rSHzVGB2": "https://elanodes.com/wp-content/uploads/custom/images/SunnyFengHan.png",
+        "iqHaEoHQNdsRBFHNXEoiwF8hMRAikgMuxS": "https://elanodes.com/wp-content/uploads/custom/images/DonaldBullers.png",
+        "if4ApisvFmMQTsBBeibYj8RYY8T6zKU5v5": "https://elanodes.com/wp-content/uploads/custom/images/ElationStudios2.png",
+        "idzud676zaw6hbSbvkfzKnZgWdK9Pj3w8T": "https://elanodes.com/wp-content/uploads/custom/images/MarkXing.png",
+        "ipLeeiAP46JHN12sXAAr22oowHJ23x9FRM": "https://elanodes.com/wp-content/uploads/custom/images/BrittanyKaiser.png",
+        "iaBNozEmoTkzeEAjrUJyZDtuX8HJHBhYtx": "https://elanodes.com/logos/Starfish.png",
+        "ifFGHBuoAbT4Hk6uHn8vsQn5et7KExyMUZ": "https://elanodes.com/wp-content/uploads/custom/images/SjunSong.png",
+        "icaVPz8nY7Y7LKjpJxmzWxCG5F3CEV6hnt": "https://elanodes.com/wp-content/uploads/custom/images/RebeccaZhu.png",
+        "ioe6q6iXHvMEmdBEB4wpd1WGyrgEuttw93": "https://elanodes.com/wp-content/uploads/custom/images/TheStrawberryCouncil_1.png",
+        "ianpxAxfvEwX2VrScpHgtBiUsSu2wcxj4B": "https://elanodes.com/wp-content/uploads/custom/images/ZhangFeng_2.png",
+        "iXMsb6ippqkCHN3EeWc4QCA9ySnrSgLc4u": "https://elanodes.com/wp-content/uploads/custom/images/NiuJingyu.png",
+        "iTN9V9kaBewdNE9aw7DfqHgRn6NcDj8HCf": "https://elanodes.com/wp-content/uploads/custom/images/Orchard1.png",
+    }
+
     async init() {
         this.initData();
     }
 
     async initData() {
+        this.elaRpcApi = this.globalElastosAPIService.getApiUrl(ElastosApiUrlType.ELA_RPC);
+        this.crRpcApi = this.globalElastosAPIService.getApiUrl(ElastosApiUrlType.CR_RPC);
+
         this.candidates = [];
         this.crmembers = [];
         this.selectedCandidates = [];
+        this.selectedMember = null;
 
         this.fetchCandidates();
         this.getSelectedCandidates();
@@ -96,6 +136,9 @@ export class CandidatesService {
             this.crmembers = result.crmembersinfo;
 
             for (let member of this.crmembers) {
+                if (!this.isCRMember && member.did == GlobalDIDSessionsService.signedInDIDString) {
+                    this.isCRMember = true;
+                }
                 member.avatar = await this.getAvatar(member.did);
             }
         }
@@ -150,34 +193,75 @@ export class CandidatesService {
         this.fetchCRMembers();
     }
 
-    private getCRCouncilTermEndpoint(): string {
-        return this.globalElastosAPIService.getApiUrl(ElastosApiUrlType.CR_RPC) + '/api/council/term';
+    async fetchCouncilTerm() {
+        try {
+            let result = await this.jsonRPCService.httpGet(this.crRpcApi + "/api/council/term");
+            Logger.log('crcouncil', 'Council terms fetched', result);
+            if (result && result.data) {
+                this.councilTerm = result.data;
+                for (var i = 0; i < this.councilTerm.length; i ++) {
+                    if (this.councilTerm[i]) {
+                        if (this.councilTerm[i].status == "CURRENT") {
+                            this.currentTermIndex = i;
+                        }
+                        else if (this.councilTerm[i].status == "VOTING") {
+                            this.votingTermIndex = i;
+                        }
+                    }
+                }
+            }
+            else {
+                Logger.error('crcouncil', 'can not get council term data!');
+            }
+        }
+        catch (err) {
+            Logger.error(App.CRCOUNCIL_VOTING, 'fetchCouncilTerm error:', err);
+        }
     }
 
-    fetchCouncilTerm() {
-        return new Promise<void>((resolve, reject) => {
-            this.http.get<any>(this.getCRCouncilTermEndpoint()).subscribe((res) => {
-                Logger.log('crcouncil', 'Council terms fetched', res);
-                if (res && res.data && res.data[0]) {
-                    this.councilTerm = res.data[0].startDate;
-                } else {
-                    Logger.error('crcouncil', 'can not get council term data!');
-                }
-                resolve();
-            }, (err) => {
-                Logger.error('crcouncil', 'fetchCouncilTerm error:', err);
-                resolve();
-            });
-        });
-    }
+    // async fetchCurrentCRMembers() {
+    //     this.crmembers = [];
+
+    //     try {
+    //         let result = await this.jsonRPCService.httpGet(this.crRpcApi + "/api/council/list/" + this.currentTermIndex);
+    //         Logger.log(App.CRCOUNCIL_VOTING, 'Fetching Current CRMembers:', result);
+    //         if (result && result.data && result.data.council) {
+    //             this.crmembers = result.data.council;
+    //             for (let member of this.crmembers) {
+    //                 if (!member.avatar) {
+    //                     member.avatar = await this.getAvatar(member.did);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     catch (err) {
+    //         Logger.error(App.CRCOUNCIL_VOTING, 'fetchCouncilTerm error:', err);
+    //     }
+    // }
 
     public async getAvatar(didString: string): Promise<string> {
-        let doc = await DIDDocument.getDIDDocumentFromDIDString(didString);
-        if (doc == null) {
-            return null;
+        if (this.avatarList[didString]) {
+            return this.avatarList[didString];
         }
 
-        return doc.getAvatar();
+        let ret = this.getAvatarFromDIDDocument(didString);
+        if (ret != null) {
+            return ret;
+        }
+
+        return "/assets/crcouncilvoting/icon/avatar.png";
+    }
+
+    public async getAvatarFromDIDDocument(didString: string): Promise<string> {
+        let doc = await DIDDocument.getDIDDocumentFromDIDString(didString);
+        if (doc != null) {
+            let ret = doc.getAvatar();
+            if (ret != null) {
+                return ret;
+            }
+        }
+
+        return null;
     }
 
     async alertErr(err: string) {
@@ -209,6 +293,23 @@ export class CandidatesService {
         });
 
         await toast.present();
+    }
+
+    async getCRMemeberInfo(did: string) {
+        try {
+            this.selectedMember = null;
+            let result = await this.jsonRPCService.httpGet(this.crRpcApi + '/api/council/information/' + did);
+            if (result && result.data) {
+                let member = result.data;
+                member.avatar = await this.getAvatar(member.did);
+                member.isSelf = member.did == GlobalDIDSessionsService.signedInDIDString;
+                this.selectedMember = member as CRMemberInfo;
+                Logger.log(App.CRCOUNCIL_VOTING, 'Selected CRMembers:', member);
+            }
+        }
+        catch (err) {
+            Logger.error(App.CRCOUNCIL_VOTING, 'Get council information error:', err);
+        }
     }
 
 }
--- src/app/crcouncilvoting/services/init.service.ts
+++ src/app/crcouncilvoting/services/init.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { App } from "src/app/model/app.enum"
+import { App } from 'src/app/model/app.enum';
 import { VoteService } from 'src/app/vote/services/vote.service';
+import { CandidatesService } from './candidates.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,14 @@ import { VoteService } from 'src/app/vote/services/vote.service';
 export class CRCouncilVotingInitService {
   constructor(
     public voteService: VoteService,
+    public candidatesService: CandidatesService,
   ) {}
 
   public async init(): Promise<void> {
   }
 
   public async start() {
+    await this.candidatesService.init();
     await this.voteService.selectWalletAndNavTo(App.CRCOUNCIL_VOTING, '/crcouncilvoting/candidates');
   }
 }
