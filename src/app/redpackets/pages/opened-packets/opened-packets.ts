5f65f23ffe94a17987a97474b7632f53fc7a7b0e
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2022-01-20 14:49:18 +0800

    CU-23zv01v [RedPackets] View all screens

--- src/app/redpackets/pages/opened-packets/opened-packets.ts
+++ src/app/redpackets/pages/opened-packets/opened-packets.ts
@@ -1,8 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 import { TitleBarComponent } from 'src/app/components/titlebar/titlebar.component';
 import { TitleBarForegroundMode } from 'src/app/components/titlebar/titlebar.types';
+import { App } from 'src/app/model/app.enum';
 import { GlobalNavService } from 'src/app/services/global.nav.service';
+import { Packet } from '../../model/packets.model';
 import { PacketService } from '../../services/packet.service';
 
 @Component({
@@ -14,18 +17,32 @@ export class OpenedPacketsPage {
   // UI components
   @ViewChild(TitleBarComponent, { static: true }) titleBar: TitleBarComponent;
 
+  // Model
+  public packets: Packet[] = [];
+
   constructor(
     public navCtrl: NavController,
     private globalNavService: GlobalNavService,
-    public packetService: PacketService
+    public packetService: PacketService,
+    private translate: TranslateService
   ) { }
 
   ionViewWillEnter() {
-    this.titleBar.setTitle("Opened Packets");
-    this.titleBar.setBackgroundColor("#f04141");
+    this.titleBar.setTitle(this.translate.instant("redpackets.opened-packets"));
+    this.titleBar.setBackgroundColor("#701919");
     this.titleBar.setForegroundMode(TitleBarForegroundMode.LIGHT);
+
+    this.packets = this.packetService.getOpenedPackets();
   }
 
   ionViewWillLeave() {
+  }
+
+  public openPacketDetails(packet: Packet) {
+    void this.globalNavService.navigateTo(App.RED_PACKETS, "/redpackets/packet-details", {
+      state: {
+        packet: packet
+      }
+    });
   }
 }
--- src/app/redpackets/pages/public-packets/public-packets.ts
+++ src/app/redpackets/pages/public-packets/public-packets.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 import { TitleBarComponent } from 'src/app/components/titlebar/titlebar.component';
 import { TitleBarForegroundMode } from 'src/app/components/titlebar/titlebar.types';
 import { App } from 'src/app/model/app.enum';
@@ -8,11 +9,11 @@ import { Packet } from '../../model/packets.model';
 import { PacketService } from '../../services/packet.service';
 
 @Component({
-  selector: 'page-my-packets',
+  selector: 'page-public-packets',
   templateUrl: 'public-packets.html',
   styleUrls: ['./public-packets.scss'],
 })
-export class MyPacketsPage {
+export class PublicPacketsPage {
   // UI components
   @ViewChild(TitleBarComponent, { static: true }) titleBar: TitleBarComponent;
 
@@ -22,15 +23,16 @@ export class MyPacketsPage {
   constructor(
     public navCtrl: NavController,
     private globalNavService: GlobalNavService,
-    public packetService: PacketService
+    public packetService: PacketService,
+    private translate: TranslateService
   ) { }
 
   ionViewWillEnter() {
-    this.titleBar.setTitle("Public Packets");
-    this.titleBar.setBackgroundColor("#f04141");
+    this.titleBar.setTitle(this.translate.instant("redpackets.public-packets"));
+    this.titleBar.setBackgroundColor("#701919");
     this.titleBar.setForegroundMode(TitleBarForegroundMode.LIGHT);
 
-    this.packets = this.packetService.getMyPackets();
+    this.packets = this.packetService.publicPackets.value;
   }
 
   ionViewWillLeave() {
