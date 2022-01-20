e7bbd28cb484435528f4167b974137c8ec5a0f5e
xuliting <xuliting_2008@yeah.net>
2022-01-21 00:03:58 +0800

    update the translation

--- translations/strings/redpackets/zh.ts
+++ translations/strings/redpackets/zh.ts
@@ -17,7 +17,7 @@ export const zh = {
     'n-days-left': "剩余 {{ days }} 天",
     'enter-captcha-placeholder': '输入验证码以打开红包',
     'wrong-captcha': '验证码错误，请重试',
-    'error-captcha-is-required': '请键入验证码以继续',
+    'error-captcha-is-required': '请输入验证码以继续',
 
     /*******
     * Home *
