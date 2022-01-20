212aa6e52decd38d72c100145dfe6f4562c7263b
raozhiming <raozhiming@elastos.org>
2022-01-20 11:39:29 +0800

    Revert "#22h2yr4 [CR Proposal] Support Proposal Type  on proposal detail"
    This reverts commit 59324fca57b804ea00d53df575508cb3689f2e55.

--- src/app/wallet/model/wallets/elastos/mainandidchain.subwallet.ts
+++ src/app/wallet/model/wallets/elastos/mainandidchain.subwallet.ts
@@ -451,20 +451,6 @@ export abstract class MainAndIDChainSubWallet extends StandardSubWallet<ElastosT
         );
     }
 
-    public async createReceiveCustomIDTransaction(payload: string, memo = ""): Promise<string> {
-        let au = await this.getAvailableUtxo(20000);
-        if (!au.utxo) return;
-
-        return this.masterWallet.walletManager.spvBridge.createReceiveCustomIDTransaction(
-            this.masterWallet.id,
-            this.id,
-            JSON.stringify(au.utxo),
-            payload,
-            '10000',
-            memo
-        );
-    }
-
     //
     //dpos registration transaction functions
     //
