3ffabf166209a90d193657ec0877a2feaed71fbd
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2022-02-28 12:44:39 +0800

    Moved getAddresses() from evmsafe to ethsafe

--- src/app/wallet/model/networks/ethereum/safes/eth.safe.ts
+++ src/app/wallet/model/networks/ethereum/safes/eth.safe.ts
@@ -3,6 +3,20 @@ import { SignTransactionResult } from "../../../safes/safe.types";
 import { EVMSafe } from "../../evms/safes/evm.safe";
 
 export class ETHSafe extends EVMSafe {
+  public getAddresses(): Promise<string[]> {
+    // TODO: Move this code to each network, because not all EVM networks support ledger,
+    // and they use a different ledger app each time (AppEth)
+
+    /* Get the address:
+    this.transport = await BluetoothTransport.create();
+    const eth = new AppEth(this.transport);
+    const path = "44'/60'/0'/0/0"; // HD derivation path
+    const r = await eth.getAddress(path, true, true); // user need open eth app and approve in ledger device.
+    const address = eip55.encode(r.address);
+    Logger.warn('ledger', "address :", address); */
+    return null;
+  }
+
   public personalSign(): string {
     throw new Error("Method not implemented.");
   }
