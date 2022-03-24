471cbd711c687519ad8645912a660d317f6cec8d
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2022-03-24 10:55:40 +0800

    Updated the download script to optionally use a download proxy to speedup github downloads from china

--- scripts/download_3rdparty.js
+++ scripts/download_3rdparty.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // CONFIGURE HERE
-const files_to_download  = [
+const files_to_download = [
   {
     "url": "https://github.com/elastos/Elastos.Essentials.Plugins.Wallet/releases/download/spvsdk-V2.10/libspvsdk.zip",
     "filename": "libspvsdk.zip",
@@ -15,12 +15,15 @@ const files_to_download  = [
 // no need to configure below
 
 const fs = require('fs'),
-      path = require('path');
+  path = require('path');
+const { env } = require('process');
+
+const proxyEnvVarName = "ESSENTIALS_TOOLCHAIN_DOWNLOAD_PROXY";
 
 function DeleteDirectory(dir) {
   if (fs.existsSync(dir) == true) {
     var files = fs.readdirSync(dir);
-    files.forEach(function(item){
+    files.forEach(function (item) {
       var item_path = path.join(dir, item);
       if (fs.statSync(item_path).isDirectory()) {
         DeleteDirectory(item_path);
@@ -33,7 +36,7 @@ function DeleteDirectory(dir) {
   }
 }
 
-module.exports = function(ctx) {
+module.exports = function (ctx) {
   // console.log("download_3rdparty ", JSON.stringify(ctx, null, 2));
 
   // make sure ios platform is part of platform add
@@ -41,16 +44,29 @@ module.exports = function(ctx) {
     return;
   }
 
-  const wget = require('node-wget-promise'),
-        readline = require('readline'),
-        md5File = require('md5-file'),
-        yauzl = require("yauzl"),
-        mkdirp = require("mkdirp");
+  //const wget = require('node-wget-promise'),
+  const readline = require('readline');
+  const md5File = require('md5-file');
+  const yauzl = require("yauzl");
+  const mkdirp = require("mkdirp");
+  const request = require("request");
+  require("colors");
 
   let cachePath = path.join(path.dirname(ctx.scriptLocation), 'cache');
   mkdirp.sync(cachePath);
 
-  let promise = new Promise(function(resolve, reject) {
+  // Proxy info
+  let proxy = undefined;
+  if (!(proxyEnvVarName in env)) {
+    console.log(`NOTE: If downloads are slow, you can configure a proxy by setting ${proxyEnvVarName} in your ENV.`.cyan);
+    console.log(`Eg: export ${proxyEnvVarName}=socks5://127.0.0.1:1080`.cyan);
+  }
+  else {
+    proxy = env[proxyEnvVarName];
+    console.log(`Using download proxy ${proxy}`.green);
+  }
+
+  let promise = new Promise(function (resolve, reject) {
     (async () => {
       let zip_file_count = 0;
       let downloaded_all_files = false;
@@ -61,8 +77,8 @@ module.exports = function(ctx) {
         // Check the md5 of the downloaded file
         //
         let fileMatched = fs.existsSync(zipFilePath)
-                          && fs.lstatSync(zipFilePath).isFile()
-                          && await md5File(zipFilePath) == obj.md5
+          && fs.lstatSync(zipFilePath).isFile()
+          && await md5File(zipFilePath) == obj.md5
 
         const max_attempt = 1;
         let attempt = 0;
@@ -71,49 +87,39 @@ module.exports = function(ctx) {
           attempt++;
 
           console.log("Starting to download file " + obj.url);
-          let unit = "bytes"
-          await wget(obj.url, {
-            onProgress: (status) => {
-              let downloadedSizeInUnit = status.downloadedSize
-              switch (unit) {
-                case "bytes":
-                  if (status.downloadedSize > (1 << 10)) {
-                      downloadedSizeInUnit /= (1 << 10)
-                      unit = "KB"
-                  }
-                  break;
-                case "KB":
-                  downloadedSizeInUnit /= (1 << 10)
-                  if (status.downloadedSize > (1 << 20)) {
-                      downloadedSizeInUnit /= (1 << 10)
-                      unit = "MB"
-                  }
-                  break;
-                case "MB":
-                  downloadedSizeInUnit /= (1 << 20)
-                  if (status.downloadedSize > (1 << 30)) {
-                      downloadedSizeInUnit /= (1 << 10)
-                      unit = "GB"
-                  }
-                  break;
-                default:
-                  downloadedSizeInUnit /= (1 << 30)
-                  break;
-              }
+
+          let downloadBuffer = Buffer.from([]);
+          let totalLength = -1;
+          await new Promise(resolve => {
+            request(obj.url, {
+              proxy
+            }, (err, result) => {
               readline.clearLine(process.stdout, 0);
-              process.stdout.write("Downloading " + downloadedSizeInUnit.toFixed(1)
-                                  + " " + unit);
-              if (status.percentage) {
-                process.stdout.write(" (" + (status.percentage * 100).toFixed(1) + "%)\r");
+
+              if (!err)
+                console.log("Download finished.");
+              else
+                console.error("Download failed.");
+
+              resolve();
+            }).on("data", data => {
+              downloadBuffer = Buffer.concat([downloadBuffer, data]);
+
+              readline.clearLine(process.stdout, 0);
+              process.stdout.write("Downloaded " + downloadBuffer.byteLength + " bytes");
+              if (totalLength !== -1) {
+                process.stdout.write(" (" + (downloadBuffer.byteLength / totalLength * 100).toFixed(1) + "%)\r");
               }
               else {
                 process.stdout.write("\r");
               }
-            },
-            output: zipFilePath
+
+            }).on("response", resp => {
+              totalLength = resp.headers['content-length'] || -1;
+            }).on("complete", () => {
+              fs.writeFileSync(zipFilePath, downloadBuffer);
+            });
           });
-          readline.clearLine(process.stdout, 0);
-          console.log("Download finished.");
 
           if (fs.existsSync(zipFilePath) && fs.lstatSync(zipFilePath).isFile()) {
             let downloadFilemd5 = await md5File(zipFilePath)
@@ -149,7 +155,7 @@ module.exports = function(ctx) {
           }
           if (fs.existsSync(targetPath) && fs.lstatSync(targetPath).isDirectory()) {
             console.log("Unziping file %s", obj.filename);
-            yauzl.open(zipFilePath, {lazyEntries: true}, function(err, zipfile) {
+            yauzl.open(zipFilePath, { lazyEntries: true }, function (err, zipfile) {
               if (err) reject(new Error(err));
               zip_file_count++;
               zipfile.readEntry();
@@ -172,9 +178,9 @@ module.exports = function(ctx) {
                       let outputPath = path.join(targetPath, relativePath);
                       mkdirp.sync(outputDir);
                       openedReadStream = true;
-                      await zipfile.openReadStream(entry, function(err, readStream) {
+                      await zipfile.openReadStream(entry, function (err, readStream) {
                         if (err) reject(new Error(err));
-                        readStream.on("end", function() {
+                        readStream.on("end", function () {
                           zipfile.readEntry();
                         });
                         let writeStream = fs.createWriteStream(outputPath);
