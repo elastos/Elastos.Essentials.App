1927aa9ba8b2fe5593e796c48a3d0846ff351146
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2022-03-28 11:37:21 +0800

    CU-2gp6t34 [Identity] Mnemonic keypad: Enter 'ji' but the page displays (jiu jing jin)

--- src/app/services/mnemonickeypad/chinese.provider.ts
+++ src/app/services/mnemonickeypad/chinese.provider.ts
@@ -3,17 +3,20 @@ import { MnemonicSuggestionProvider } from "./suggestionprovider";
 
 export class ChineseMnemonicSuggestionProvider implements MnemonicSuggestionProvider {
   private mapping: AsciiMapping;
+  private orderedKeys: string[];
 
   constructor() {
     void import("src/assets/components/mnemonic-keypad/simplified_chinese.json").then(mapping => {
       this.mapping = mapping.default as any as AsciiMapping;
+      let keys = Object.keys(this.mapping);
+      this.orderedKeys = keys.sort((a, b) => a.localeCompare(b));
     });
   }
 
   getSuggestions(letters: string): string[] {
     letters = letters.toLowerCase();
 
-    let matchingKeys = Object.keys(this.mapping).filter(key => key.startsWith(letters));
+    let matchingKeys = this.orderedKeys.filter(key => key.startsWith(letters));
     return matchingKeys.reduce((acc, key) => [...acc, ...(this.mapping[key] || [])], [] as string[]).slice(0, 40);
   }
 }
