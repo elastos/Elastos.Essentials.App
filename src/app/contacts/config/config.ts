086e3b4a7047c38cb524ad138d38adcc6ccf58d3
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2022-07-15 19:27:49 +0800

    Added sash and elacity to default contacts

--- src/app/contacts/config/config.ts
+++ src/app/contacts/config/config.ts
@@ -2,8 +2,11 @@ export const defaultContacts = [
   'did:elastos:iabbGwqUN18F6YxkndmZCiHpRPFsQF1imT', // Chen Rong
   'did:elastos:iXyYFboFAd2d9VmfqSvppqg1XQxBtX9ea2', // Trinity Tech
   'did:elastos:insTmxdDDuS9wHHfeYD1h5C2onEHh3D8Vq', // Benjamin Piette, Trinity Tech
-  'did:elastos:imxNkhKuuXaefyFKQuzFnkfRdedDVLYmKV', // Tuum Tech
-  'did:elastos:iXEgnmYTbFGSsVf16gkVcrGuK3Nv2vGTMD', // Donald Bullers, Tuum Tech
+  //'did:elastos:imxNkhKuuXaefyFKQuzFnkfRdedDVLYmKV', // Tuum Tech
+  //'did:elastos:iXEgnmYTbFGSsVf16gkVcrGuK3Nv2vGTMD', // Donald Bullers, Tuum Tech
   'did:elastos:ijQcPGNKAUWQEwoTPTHqLwGeBVS6CidePP', // Michael S, community, moderator
-  'did:elastos:in2Zy5UqvdgGY41149cE5UTJEzgSdcf558', // Ryan, Elanodes, Elavate DEX, community.
+  'did:elastos:in2Zy5UqvdgGY41149cE5UTJEzgSdcf558', // Ryan, Elanodes, Glide Finance DEX, community.
+  'did:elastos:iZM2HZhHnzWKbQWBcbvLk1nFa4EqUEJzrr', // MButcho, community member, ELAB
+  'did:elastos:ik9HwavTJhdo2ufc1K7Gz7pLF4yraaVcCR', // Sash
+  'did:elastos:inpCykPj1JMroJfjFvDAGnEGUCGGG3ptwQ' // Elacity
 ];
