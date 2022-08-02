2e3a3700afd579f15f77323b50163cb2a2fb8195
raozhiming <raozhiming@elastos.org>
2022-08-02 16:22:28 +0800

    fix archive error in ios release version

---
+++ scripts/firebaseDeploymentTargetFix.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+// Workaround for https://github.com/dpa99c/cordova-plugin-firebasex/issues/735
+// FirebaseFirestore appears to require iOS deployment target >= 11.0
+
+const fs = require("fs");
+const path = require("path");
+const execa = require("execa");
+
+module.exports = (context) => {
+  const platformPath = path.resolve(context.opts.projectRoot, "platforms/ios");
+  const podfilePath = path.resolve(platformPath, "Podfile");
+
+  if (!fs.existsSync(podfilePath)) {
+    console.log(`'${podfilePath}' does not exist. Firebase deployment fix skipped.`);
+    return;
+  }
+
+  let podfileContent = fs.readFileSync(podfilePath, "utf-8");
+  if (podfileContent.indexOf("post_install") == -1) {
+    podfileContent += `
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    if target.name == 'FirebaseFirestore'
+      target.build_configurations.each do |config|
+        config.build_settings.delete 'IPHONEOS_DEPLOYMENT_TARGET'
+      end
+    end
+  end
+end`;
+
+    fs.writeFileSync(podfilePath, podfileContent, "utf-8");
+
+    return execa("pod", ["install", "--verbose"], {
+      cwd: platformPath,
+    });
+  }
+};
