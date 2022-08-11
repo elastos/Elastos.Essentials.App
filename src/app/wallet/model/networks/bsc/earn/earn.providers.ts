e3c19b8fb44ac69aec3b366c8e56c4dc18dbd154
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2022-08-11 12:09:35 +0800

    CU-2ptjxct Update oELK to ELK for swap/earn/bridge

--- src/app/wallet/model/networks/bsc/earn/earn.providers.ts
+++ src/app/wallet/model/networks/bsc/earn/earn.providers.ts
@@ -7,7 +7,8 @@ export const bscMainnetElkEarnProvider = new EarnProvider(elkBaseProvider,
   ],
   // additional coins - must be lowercase
   [
-    "0xe1c110e1b1b4a1ded0caf3e42bfbdbb7b5d7ce1c" // ELK token itself
+    "0xe1c110e1b1b4a1ded0caf3e42bfbdbb7b5d7ce1c", // oELK
+    "0xeeeeeb57642040be42185f49c52f7e9b38f8eeee" // ELK
   ],
   "https://app.elk.finance/#/stake"
 );
--- src/app/wallet/model/networks/telos/earn/bridge.providers.ts
+++ src/app/wallet/model/networks/telos/earn/bridge.providers.ts
@@ -5,6 +5,7 @@ export const telosMainnetElkBridgeProvider = new BridgeProvider(
   elkBaseProvider,
   false,
   [
-    "0xe1c110e1b1b4a1ded0caf3e42bfbdbb7b5d7ce1c", // ELK
+    "0xe1c110e1b1b4a1ded0caf3e42bfbdbb7b5d7ce1c", // oELK
+    "0xeeeeeb57642040be42185f49c52f7e9b38f8eeee" // ELK
   ]
 );
