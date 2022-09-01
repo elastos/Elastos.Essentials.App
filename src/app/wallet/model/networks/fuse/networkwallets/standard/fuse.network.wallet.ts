5bbb4aae8f2a2ea9bfc0714cd0a4fcc6889b072d
raozhiming <raozhiming@elastos.org>
2022-09-01 11:52:55 +0800

    [wallet]Only support tokentx action on fuse network

--- src/app/wallet/model/networks/fuse/networkwallets/standard/fuse.network.wallet.ts
+++ src/app/wallet/model/networks/fuse/networkwallets/standard/fuse.network.wallet.ts
@@ -1,9 +1,9 @@
 import { TransactionProvider } from "../../../../tx-providers/transaction.provider";
 import { StandardEVMNetworkWallet } from "../../../evms/networkwallets/standard/standard.evm.networkwallet";
-import { EtherscanTransactionProvider } from "../../../evms/tx-providers/etherscan.tx.provider";
+import { FuseTransactionProvider } from "../../tx-providers/fuse.transaction.provider";
 
 export class FuseNetworkWallet extends StandardEVMNetworkWallet<any> {
   protected createTransactionDiscoveryProvider(): TransactionProvider<any> {
-    return new EtherscanTransactionProvider(this);
+    return new FuseTransactionProvider(this);
   }
 }
