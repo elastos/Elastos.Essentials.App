a9c5f8414e8017e00fead821fb6acb767d26b530
raozhiming <raozhiming@elastos.org>
2022-10-09 12:51:15 +0800

    rename hoo.transaction.provider.ts

--- src/app/wallet/model/networks/hoo/networkwallets/standard/hoo.network.wallet.ts
+++ src/app/wallet/model/networks/hoo/networkwallets/standard/hoo.network.wallet.ts
@@ -1,6 +1,6 @@
 import { TransactionProvider } from "../../../../tx-providers/transaction.provider";
 import { StandardEVMNetworkWallet } from "../../../evms/networkwallets/standard/standard.evm.networkwallet";
-import { HooTransactionProvider } from "../../tx-providers/heco.transaction.provider";
+import { HooTransactionProvider } from "../../tx-providers/hoo.transaction.provider";
 
 export class HooNetworkWallet extends StandardEVMNetworkWallet<any> {
   protected createTransactionDiscoveryProvider(): TransactionProvider<any> {
