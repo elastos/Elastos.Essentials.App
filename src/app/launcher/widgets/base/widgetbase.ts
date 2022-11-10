134a9c9593cc86da1b2cb8543117ddd33da0b00c
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2022-11-10 11:56:33 +0800

    CU-37cawj1 Fixed spinning home screen after deleting a widget

--- src/app/launcher/widgets/base/widgetbase.ts
+++ src/app/launcher/widgets/base/widgetbase.ts
@@ -5,6 +5,7 @@ import type { WidgetState } from "./widgetstate";
 export class WidgetBase {
   public widgetState: WidgetState = null;
 
+  private alreadyNotifiedReadyToDisplay = false; // Make sure to notify ready to display only once to avoid unnecessary work from the launcher home
   public onReadyToDisplay = new BehaviorSubject<boolean>(false);
   public forSelection: boolean; // Widget is created for selection, not for real use. It can adjust some displays accordingly for simple preview.
 
@@ -19,6 +20,10 @@ export class WidgetBase {
    * Only at the time the global container spinner will hive and display all the widgets at once.
    */
   protected notifyReadyToDisplay() {
+    if (this.alreadyNotifiedReadyToDisplay)
+      return;
+
     this.onReadyToDisplay.next(true);
+    this.alreadyNotifiedReadyToDisplay = true;
   }
 }
