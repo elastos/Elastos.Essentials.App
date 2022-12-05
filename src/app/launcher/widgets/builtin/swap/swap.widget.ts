735cce3203b3642d855875301fec93399e195b81
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2022-12-05 09:04:56 +0800

    CU-3wzqket Remove the swap widget from the default home screen on ios

--- src/app/launcher/widgets/builtin/swap/swap.widget.ts
+++ src/app/launcher/widgets/builtin/swap/swap.widget.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { customizedSVGID } from 'src/app/helpers/picture.helpers';
 import { AppmanagerService } from 'src/app/launcher/services/appmanager.service';
@@ -9,24 +10,39 @@ import { SwapUIService } from 'src/app/multiswap/services/swap.ui.service';
 import { GlobalNavService } from 'src/app/services/global.nav.service';
 import { GlobalThemeService } from 'src/app/services/theming/global.theme.service';
 import { WidgetBase } from '../../base/widgetbase';
+import { WidgetsServiceEvents } from '../../services/widgets.events';
 
 @Component({
   selector: 'widget-swap',
   templateUrl: './swap.widget.html',
   styleUrls: ['./swap.widget.scss'],
 })
-export class SwapWidget extends WidgetBase {
+export class SwapWidget extends WidgetBase implements OnInit {
   public sourceToken: UIToken = null;
   public destinationToken: UIToken = null;
+  public isIOS = false;
+  public editing: boolean; // Widgets container is being edited
 
   constructor(
     public theme: GlobalThemeService,
     private translate: TranslateService,
     public appService: AppmanagerService,
     private nav: GlobalNavService,
-    private swapUIService: SwapUIService
+    private swapUIService: SwapUIService,
+    private platform: Platform
   ) {
     super();
+  }
+
+  ngOnInit() {
+    // For now (3.0.0 release), remove the swap widgets on iOS as apple complains about built-in swaps.
+    // We can try to disable this ios check later (with changes to get rejected).
+    this.isIOS = this.platform.platforms().indexOf('android') < 0;
+
+    // Watch edition mode change to show this widget in edition even if not showing in live mode.
+    WidgetsServiceEvents.editionMode.subscribe(editing => {
+      this.editing = editing;
+    });
 
     this.notifyReadyToDisplay();
   }
