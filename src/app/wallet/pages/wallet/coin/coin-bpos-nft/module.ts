b094a6d87a48486bac34c5d02075ee64dee5d7df
pixel <pixel@escription.net>
2024-08-06 17:26:23 +0800

    [wallet][BPoSNFT]Show reward info on bpos nft detail screen

--- src/app/wallet/pages/wallet/coin/coin-bpos-nft/module.ts
+++ src/app/wallet/pages/wallet/coin/coin-bpos-nft/module.ts
@@ -7,6 +7,8 @@ import { TranslateModule } from '@ngx-translate/core';
 import { InlineSVGModule } from 'ng-inline-svg-2';
 import { SharedComponentsModule } from 'src/app/components/sharedcomponents.module';
 import { CoinBPoSNFTPage } from './coin-bpos-nft.page';
+import { ComponentsModule } from 'src/app/wallet/components/components.module';
+import { EthTransactionComponentModule } from 'src/app/wallet/components/eth-transaction/module';
 
 @NgModule({
     declarations: [CoinBPoSNFTPage],
@@ -16,6 +18,8 @@ import { CoinBPoSNFTPage } from './coin-bpos-nft.page';
         FormsModule,
         IonicModule,
         TranslateModule,
+        ComponentsModule,
+        EthTransactionComponentModule,
         InlineSVGModule,
         RouterModule.forChild([{ path: '', component: CoinBPoSNFTPage }])
     ],
